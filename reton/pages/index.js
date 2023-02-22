f8ba00a444cce887cf0530cdf406c4f766477336
eduardo.bautista <eduardosanzb@gmail.com>
2023-02-22 21:33:27 +0100

    Fix

--- reton/pages/index.js
+++ reton/pages/index.js
@@ -9,11 +9,11 @@ import Footer from "../components/LandingPageOne/Footer";
 import Eduardo from "../eduardo.config";
 import Ave from "../ave.config";
 
-const config = process.env.WHO === 'Ave' ? Ave : Eduardo
-const imgUrl = process.env.WHO === 'Ave' ? 'ave' : 'banner-main1'
-console.log(config)
-console.log(process.env.WHO)
+
 const Index = () => {
+const config = process.env.NEXT_PUBLIC_WHO === 'Ave' ? Ave : Eduardo
+const imgUrl = process.env.NEXT_PUBLIC_WHO === 'Ave' ? 'ave' : 'banner-main1'
+
   return (
     <React.Fragment>
       <Navbar config={config.Navbar} />
