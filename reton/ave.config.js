d9315fbf25ffefd9dc19fc508f7322263faec7a4
AveVigil <126063979+AveVigil@users.noreply.github.com>
2023-02-22 20:25:06 +0000

    Cambio

--- reton/ave.config.js
+++ reton/ave.config.js
@@ -27,13 +27,11 @@ const config = {
   About: {
     title: "I'm from Mexico and I love CrossFit, reading, hiking & 🌮",
     description: `
-    I was born in 1991 and grew up in Puebla, Mexico.
+    <p>I was born in 1991 and grew up in Puebla, Mexico.
     After I finished studying as an automotive design engineer, I started working on projects for Volkswagen. 
-    Then, I joined Lightyear's mission, where I helped develop the world's first long-range solar electric vehicle that's production-ready.
-
-
-    Outside of work you can find me in the GYM or at CorssFit competitions.
-    <--- That's me doing (and almost dying in the process) a 140 kilos clean. 
+    Then, I joined Lightyear's mission, where I helped develop the world's first long-range solar electric vehicle that's production-ready.</p>
+    <p>Outside of work you can find me in the GYM or at CorssFit competitions.</p>
+    <p><--- That's me doing (and almost dying in the process) a 140 kilos clean.</p> 
     `,
     residence: "Eindhoven",
     addiction: "☕️ (ESPRESSO)",
